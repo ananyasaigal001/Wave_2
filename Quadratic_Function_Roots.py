f8e25a26b46e295b6719ed08e6a8cc46ffbe709f
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-04-16 01:47:10 -0400

    Roots of quadratic function

--- Quadratic_Function_Roots.py
+++ Quadratic_Function_Roots.py
@@ -1,10 +1,13 @@
 import math
+
 #obtain the values
 a = int(input("Enter the a value: "))
 b = int(input("Enter the b value: "))
 c = int(input("Enter the c value: "))
+
 #determine the discriminant
 discriminant=((b**2)-4*a*c)
+
 #determine the roots
 if discriminant>0:
     root_one=(-b+math.root(dicriminant))/(2*a)
