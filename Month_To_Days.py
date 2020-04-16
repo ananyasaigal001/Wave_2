129fc0966e80d2d2473292af2eae722cb23f86c7
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-04-16 01:48:51 -0400

    Months to days

--- Month_To_Days.py
+++ Month_To_Days.py
@@ -4,12 +4,9 @@ month=str(input("Enter a month: "))
 #Determine the number of days in the month
 if month=="February":
     print("There are 28 or 29 days")
-
 elif month=="April"or "June"or "September"or"November" :
     print("There are 30 days")
-
 elif month== "January" or "March"or "May"or "July" or "August"or "October"or "December":    
     print("There are 31 days")
- 
 else:
     print("Error")
